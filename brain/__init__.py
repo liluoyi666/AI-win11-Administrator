41b8c62e09c84c6eb4202f71320e18b8f5826bde
liluoyi666 <1654970980@qq.com>
2025-05-30 09:27:03 +0800

    xxx

--- brain/__init__.py
+++ brain/__init__.py
@@ -1,4 +1,4 @@
-from .executor import PowerShellSession     # powershell服务
+from .powershell import PowerShellSession     # powershell服务
 from .String_Templates import system_prompt,grammar,user_msg,error_msg  # 字符串模板
 from .LLM_api import create_client,get_response_from_llm    # LLM的api服务
 from .json_parser import extract_json_between_markers       # json解析器
